7d9ac9ffe73c1419222a1d0163dc4db56b5fc76a
Joshua Habif <38256075+Joshua-Habif@users.noreply.github.com>
2019-05-07 18:19:03 -0400

    Update FIFOComparator.java

--- DemandPaging/src/FIFOComparator.java
+++ DemandPaging/src/FIFOComparator.java
@@ -4,6 +4,10 @@ public class FIFOComparator implements Comparator<Pair>{
 	
 	@Override
 	public int compare(Pair o1, Pair o2) {
-		return (-1*o1.compareTo(o2));
+		if(o1.getLoadTime()>o2.getLoadTime())
+			return 1;
+		if(o1.getLoadTime()<o2.getLoadTime())
+			return -1;
+		return 0;
 	}
 }
